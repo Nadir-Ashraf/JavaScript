2465fa02da6a5658aa4bc28f29b491c62880c893
Nadir Ashraf <nadirashraf625@gmail.com>
2025-09-06 23:21:48 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log(`Hello world`);	//Backticks - Template literals
+console.log("Hello world"); //Double quotes
+console.log('Hello world');	//Single quotes
+
+// window.alert("This is an alert");	//Display an alert box
+
+// Single line comment
+
+/* 
+	Multi
+    Line
+    comment
+*/
+
+document.getElementById("firstH1").textContent = "Hello world!";
+document.getElementById("firstP").textContent = "This is a paragraph";
+
+
