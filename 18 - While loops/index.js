4124312011c59c2784fd1ab0d84d29fb025687e8
Nadir Ashraf <nadirashraf625@gmail.com>
2025-09-19 21:51:40 +0530

    while loops

---
+++ 18 - While loops/index.js
@@ -0,0 +1,33 @@
+// While loop = repeat some code WHILE the condition is true.
+
+let userName;
+let password;
+let isLoggedIn = false;
+
+// while(!isLoggedIn){
+//     userName = window.prompt(`Enter username`);
+//     password = window.prompt(`Enter password`);
+
+//     if(userName === "nadir" && password === "123"){
+//         console.log(`Welcome ${userName}`);
+//         isLoggedIn = true;
+//     }
+//     else{
+//         console.log("Invalid credentials!");
+//     }
+// }
+
+//------------------- do while ------------------------------------------------------------------------------------------------------
+
+// do{
+//     userName = window.prompt(`Enter username`);
+//     password = window.prompt(`Enter password`);
+
+//     if(userName === "nadir" && password === "123"){
+//         console.log(`Welcome ${userName}`);
+//         isLoggedIn = true;
+//     }
+//     else{
+//         console.log("Invalid credentials!");
+//     }
+// } while(!isLoggedIn)
