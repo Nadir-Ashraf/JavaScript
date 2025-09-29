481dd8c985283a5e8bafb86dbcf707e34c5a0646
Nadir Ashraf <nadirashraf625@gmail.com>
2025-09-29 06:44:26 +0530

    map and filter methods

---
+++ 32 - map method/map.js
@@ -0,0 +1,45 @@
+// map() = accepts a callback and applies that function to each element of an 
+//         array, then return a new array
+
+//-------------------------------------------------------------------------------------------------------------------------------------------
+
+// const numbers = [1, 2, 3, 4, 5];
+
+// function square(element){
+//     return Math.pow(element, 2);
+// }
+
+// function cube(element){
+//     return Math.pow(element, 3);
+// }
+
+// const squaredNumbers = numbers.map(square);
+// const cubedNumbers = numbers.map(cube);
+
+// console.log(squaredNumbers);
+// console.log(cubedNumbers);
+
+//-------------------------------------------------------------------------------------------------------------------------------------------
+
+// const students = ["Nadir", "Harshil", "Asharaf", "Febin"];
+
+// function upperCase(element){
+//     return element.toUpperCase();
+// }
+
+// const studentsUpperCase = students.map(upperCase);
+
+// console.log(studentsUpperCase);
+
+//--------------------------------------------------------------------------------------------------------------------------------------------
+
+// const dates = ["2023-01-10", "2024-02-24", "2025-03-12"];   // Date format - YYYY/MM/DD
+
+// function dateFormat(element){
+//     const parts = element.split("-");
+//     return `${parts[1]}/${parts[2]}/${parts[0]}`;
+// }
+
+// const formattedDates = dates.map(dateFormat);   // formatted dates - DD/MM/YYYY
+// console.log(formattedDates);
+
---
+++ 33 - filter method/filter.js
@@ -0,0 +1,27 @@
+// filter() =  creates a new array by filtering 
+//             out elements
+
+//-------------------------------------------------------------------------------------------------------------------------------------------
+
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];   // array of numbers
+
+// function isEven(element){
+//     return element % 2 === 0;
+// }
+
+// let evenNums = numbers.filter(isEven);
+// console.log(evenNums);
+
+//-------------------------------------------------------------------------------------------------------------------------------------------
+
+// let student = [16, 17, 18, 19, 19, 20];
+
+// function isAdult(element){
+//     return element >= 18;
+// }
+
+// let adults = student.filter(isAdult);   // There are 4 adults in 'students' array
+// console.log(adults);
+
+//--------------------------------------------------------------------------------------------------------------------------------------------
+
